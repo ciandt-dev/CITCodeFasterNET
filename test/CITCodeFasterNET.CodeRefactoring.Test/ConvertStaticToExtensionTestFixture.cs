2ded1bf1d27d616558ebebcc722bbed8145e0294
Fernando <fsiqueira@ciandt.com>
2014-10-14 20:22:39 -0300

    Add several tests, splitting a big test in subtests, kept one complete test, to test for multiple occurrences of refactory

--- test/CITCodeFasterNET.CodeRefactoring.Test/ConvertStaticToExtensionTestFixture.cs
+++ test/CITCodeFasterNET.CodeRefactoring.Test/ConvertStaticToExtensionTestFixture.cs
@@ -45,7 +45,166 @@ namespace CITCodeFasterNET.CodeRefactoring.Test
         }
 
         [TestMethod]
-        public void should_001()
+        public void should_refactory_when_static_call_in_same_class()
+        {
+            // Arrange
+            var testCase = TestHelper.CreateTestDocumentWithClassAditionalContent(
+StaticMethod() +
+@"
+public static void Test()
+{
+    var strTest = " + "\"My queued string.\"" + @";
+ 
+    var queuedString = ConvertToQueue(strTest);
+
+}
+");
+
+            // Act
+            var finalText = TestHelper.ApplyRefactory<ConvertStaticToExtensionProvider>("Convert to extension method", testCase);
+
+            // Assert
+            Assert.IsTrue(finalText.Contains("var queuedString = strTest.ConvertToQueue();"));
+        }
+
+
+        [TestMethod]
+        public void should_refactory_when_static_call_is_made_using_static_class()
+        {
+            // Arrange
+            var testCase = TestHelper.CreateTestDocumentWithClassAditionalContent(
+StaticMethod() +
+@"
+public static void Test()
+{
+    var strTest = " + "\"My queued string.\"" + @";
+ 
+    var test = TestClass.ConvertToQueue(strTest, 10);
+}
+");
+
+            // Act
+            var finalText = TestHelper.ApplyRefactory<ConvertStaticToExtensionProvider>("Convert to extension method", testCase);
+
+            // Assert
+            Assert.IsTrue(finalText.Contains("var test = strTest.ConvertToQueue(10);"));
+        }
+
+
+
+        [TestMethod]
+        public void should_refactory_when_static_call_is_made_as_a_parameter_using_static_class()
+        {
+            // Arrange
+            var testCase = TestHelper.CreateTestDocumentWithClassAditionalContent(
+StaticMethod() + @"
+public static void Test()
+{
+    var strTest = " + "\"My queued string.\"" + @";
+ 
+    Method(TestClass.ConvertToQueue(strTest));
+}
+
+public static void Method(Queue<char> queueChar)
+{
+    return;
+}
+");
+
+            // Act
+            var finalText = TestHelper.ApplyRefactory<ConvertStaticToExtensionProvider>("Convert to extension method", testCase);
+
+            // Assert
+            Assert.IsTrue(finalText.Contains("Method(strTest.ConvertToQueue());"));
+        }
+
+
+        [TestMethod]
+        public void should_refactory_when_static_call_is_made_as_a_parameter_using_same_class_call()
+        {
+            // Arrange
+            var testCase = TestHelper.CreateTestDocumentWithClassAditionalContent(
+StaticMethod() + 
+@"
+public static void Test()
+{
+    var strTest = " + "\"My queued string.\"" + @";
+ 
+    Method(ConvertToQueue(strTest));
+}
+
+public static void Method(Queue<char> queueChar)
+{
+    return;
+}
+");
+
+            // Act
+            var finalText = TestHelper.ApplyRefactory<ConvertStaticToExtensionProvider>("Convert to extension method", testCase);
+
+            // Assert
+            Assert.IsTrue(finalText.Contains("Method(strTest.ConvertToQueue());"));
+        }
+
+
+
+
+        [TestMethod]
+        public void should_refactory_when_static_call_is_made_as_a_parameter_using_linq()
+        {
+            // Arrange
+            var testCase = TestHelper.CreateTestDocumentWithClassAditionalContent(
+StaticMethod() + 
+@"
+public static void Test()
+{
+    var strTest = " + "\"My queued string.\"" + @";
+ 
+    MethodClone((x) => ConvertToQueue(x));
+}
+public static Queue<char> MethodClone(Func<string, Queue<char>> funcQueueChar)
+{
+    return funcQueueChar(" + "\"bla\"" + @");
+}
+");
+            // Act
+            var finalText = TestHelper.ApplyRefactory<ConvertStaticToExtensionProvider>("Convert to extension method", testCase);
+
+            // Assert
+            Assert.IsTrue(finalText.Contains("MethodClone((x) => x.ConvertToQueue());"));
+        }
+
+        [TestMethod]
+        public void should_refactory_when_static_call_is_made_as_a_parameter_using_linq_2()
+        {
+            // Arrange
+            var testCase = TestHelper.CreateTestDocumentWithClassAditionalContent(
+StaticMethod() +
+@"
+public static void Test()
+{
+    var strTest = " + "\"My queued string.\"" + @";
+ 
+    MethodClone((x) => { return ConvertToQueue(x); });
+
+}
+
+public static Queue<char> MethodClone(Func<string, Queue<char>> funcQueueChar)
+{
+    return funcQueueChar(" + "\"bla\"" + @");
+}
+");
+
+            // Act
+            var finalText = TestHelper.ApplyRefactory<ConvertStaticToExtensionProvider>("Convert to extension method", testCase);
+
+            // Assert
+            Assert.IsTrue(finalText.Contains("MethodClone((x) => { return x.ConvertToQueue(); });"));
+        }
+
+
+        [TestMethod]
+        public void should_refactor_when_code_contains_several_refactory_occurrences()
         {
             // Arrange
             var testCase = TestHelper.CreateTestDocumentWithClassAditionalContent(
@@ -61,18 +220,10 @@ namespace CITCodeFasterNET.CodeRefactoring.Test
     return result;
 }
 
-public static bool TestExtension(this string str)
-{
-    return true;
-}
-
 public static void Test()
 {
     var strTest = " + "\"My queued string.\"" + @";
-
-    //var t01 = strTest.TestExtension();
-
-    MethodClone(ConvertToQueue);
+ 
     var queuedString = ConvertToQueue(strTest);
     var test = TestClass.ConvertToQueue(strTest, 10);
     Method(TestClass.ConvertToQueue(strTest));
@@ -97,6 +248,13 @@ public static Queue<char> MethodClone(Func<string, Queue<char>> funcQueueChar)
             var finalText = TestHelper.ApplyRefactory<ConvertStaticToExtensionProvider>("Convert to extension method", testCase);
 
             // Assert
+            Assert.IsTrue(finalText.Contains(
+    @"var queuedString = strTest.ConvertToQueue();
+    var test = strTest.ConvertToQueue(10);
+    Method(strTest.ConvertToQueue());
+    Method(strTest.ConvertToQueue());
+    MethodClone((x) => x.ConvertToQueue());
+    MethodClone((x) => { return x.ConvertToQueue(); });"));
         }
 
         [TestMethod, ExpectedException(typeof(ArgumentNullException))]
@@ -118,5 +276,25 @@ public static Queue<char> MethodClone(Func<string, Queue<char>> funcQueueChar)
             // Act
             var finalText = TestHelper.ApplyRefactory<ConvertStaticToExtensionProvider>("Convert to extension method", testCase);
         }
+
+
+
+        private string StaticMethod()
+        {
+            return @"public static Queue<char> Con$vertToQueue(string str, int count = 0)
+{
+    var result = new Queue<char>();
+
+    foreach (var strChar in str)
+    {
+        result.Enqueue(strChar);
     }
+
+    return result;
+}";
+        }
+
+    }
+
+
 }
